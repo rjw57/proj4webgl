5f436d37d3272d1bc2d8b750087eb5b8db4a8d28
Rich Wareham <rjw57@cam.ac.uk>
2013-04-16 13:46:07 +0100

    implement eqc forward transform

--- proj/eqc.glsl
+++ proj/eqc.glsl
@@ -1,5 +1,19 @@
 /* similar to equi.js FIXME proj4 uses eqc */
 
+// forward equations--mapping lat,long to x,y
+// -----------------------------------------------------------------
+vec2 eqc_forwards(vec2 p, eqc_params params)
+{
+    float lon = p.x;
+    float lat = p.y;
+
+    float dlon = adjust_lon(lon - params.long0);
+    float dlat = adjust_lat(lat - params.lat0);
+    p.x = params.x0 + (params.a * dlon * params.rc);
+    p.y = params.y0 + (params.a * dlat);
+    return p;
+}
+
 // inverse equations--mapping x,y to lat/long
 // -----------------------------------------------------------------
 vec2 eqc_backwards(vec2 p, eqc_params params)
